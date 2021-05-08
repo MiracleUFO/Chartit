5f65d77fcec7e20d57cc79f0409f8374ab0b9b35
MircleUFO <miracleufodiama@gmail.com>
2021-05-08 20:43:10 +0100

    Refactor app structure

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-config.mongoURI = {
-  development: 'mongodb://localhost:27017/chartit',
-  test: 'mongodb://localhost/chartit-test',
-  production: process.env.MONGODB_URI
-};
-
-module.exports = config;
--- server/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express.Router();
-
-const AuthController = require('../controllers/AuthController');
-
-app.post('/signup', AuthController.signup);
-app.post('/login', AuthController.login);
-
-module.exports = app;
